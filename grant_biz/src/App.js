7352a79ea204f4b0064c690a4822e10d61b5a29a
warsam-abdo <warsame1724@gmail.com>
2022-10-20 17:33:13 +0800

    Add taiwind

--- grant_biz/src/App.js
+++ grant_biz/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <header className="App-header ">
+      <div className='bg-blue-500 md:bg-red-500'>hi</div>
+       {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="text-red-600">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
