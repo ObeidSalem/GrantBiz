5036865d082dea6de53bc3a6067a8e1fe2ece853
warsam-abdo <warsame1724@gmail.com>
2022-10-26 19:15:25 +0800

    create nav 2

--- grant_biz/src/App.js
+++ grant_biz/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import './App.css';
-import './styles.css'
-import HomePage from './Components/HomePage';
-import SignIn from './Components/SignIn';
-import SignUp from './Components/SignUp';
-import Profile from './Components/Profile';
-import MyShop from './Components/MyShop';
-import Product from './Components/Product';
+import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import "./App.css";
+import "./styles.css";
+import HomePage from "./Components/HomePage";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import Profile from "./Components/Profile";
+import MyShop from "./Components/MyShop";
+import Product from "./Components/Product";
 
 function App() {
   return (
@@ -15,12 +20,12 @@ function App() {
       <Router>
         <div className="content-center	">
           <Routes>
-            <Route exact path='/' element={<HomePage />} />
-            <Route path='/SignIn' element={<SignIn />} />
-            <Route path='/SignIn' element={<SignUp />} />
-            <Route path='/Profile' element={<Profile />} />
-            <Route path='/MyShop' element={<MyShop />} />
-            <Route path='/Product/:id' element={<Product />} />
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/SignIn" element={<SignUp />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/MyShop" element={<MyShop />} />
+            <Route path="/Product/:id" element={<Product />} />
           </Routes>
         </div>
       </Router>
