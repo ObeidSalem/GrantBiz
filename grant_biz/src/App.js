2e5a8f0bf9223df880a5361a832231f674ad0fa2
Obeid Salem Ahmed Bashwar <ObeidSalem@users.noreply.github.com>
2022-10-26 17:49:00 +0800

    26 oct 1

--- grant_biz/src/App.js
+++ grant_biz/src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
-// import './App.css';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import './App.css';
 import './styles.css'
+import HomePage from './Components/HomePage';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
+import Profile from './Components/Profile';
+import MyShop from './Components/MyShop';
+import Product from './Components/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header ">
-      <div className='bg-blue-500 md:bg-red-500'>hi</div>
-       {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p className="text-red-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="content-center	">
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route path='/SignIn' element={<SignIn />} />
+            <Route path='/SignIn' element={<SignUp />} />
+            <Route path='/Profile' element={<Profile />} />
+            <Route path='/MyShop' element={<MyShop />} />
+            <Route path='/Product/:id' element={<Product />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
