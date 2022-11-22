4ef7b86ff14bbc9d715c946098b38a5696acfb3e
Obeid Salem Ahmed Bashwar <ObeidSalem@users.noreply.github.com>
2022-11-22 13:39:14 +0800

    22-11-1

--- grant_biz/src/Components/HomePage.js
+++ grant_biz/src/Components/HomePage.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 
+import db from "../firebase"
+import { onSnapshot, collection, doc, setDoc, getDocs } from "firebase/firestore"
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../actions";
+
+
+
 function HomePage() {
+
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch;
+  const [useStateProducts, setUseStateProducts] = useState()
+
+  useEffect(() => {
+
+    onSnapshot(collection(db, "Products"), (snapshot) => {
+      console.log(snapshot.docs.map(doc => doc.data()));
+      setUseStateProducts(snapshot.docs.map(doc => doc.data()));
+      console.log("useStateProducts", useStateProducts);
+      dispatch(setProducts(useStateProducts))
+    })
+
+  }, []) 
+
   return (
     <>
-      <NavBar/>
-      <div></div>
+      <NavBar />
+      <div>
+
+      </div>
     </>
   );
 }
--- grant_biz/src/actions/index.js
+++ grant_biz/src/actions/index.js
@@ -1,5 +1,14 @@
+import { ActionTypes } from "../contents/action-types";
+
+
 export const signIn = () => {
     return {
         type: 'SIGN_IN',
+    }
+}
+
+export const setProducts = () => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
     }
 }
