137ef8daa66f23089d36dd3002f0283a7b59d741
warsam-abdo <warsame1724@gmail.com>
2022-12-09 23:28:30 +0800

    cancel page added

--- grant_biz/src/Components/Order/Orders.js
+++ grant_biz/src/Components/Order/Orders.js
@@ -7,6 +7,8 @@ import {
   IoLocationOutline,
   IoTrashOutline,
   IoChatbubblesOutline,
+  IoCloseCircleOutline,
+  IoCloseCircleSharp,
 } from "react-icons/io5";
 import { BsBoxSeam, BsTruck } from "react-icons/bs";
 
@@ -81,15 +83,22 @@ const Orders = () => {
     }
   }, [user]);
 
+  
+  function refreshPage() {
+    window.location.reload(false);
+}
+
   const [Category1, setCategory1] = useState(true);
   const [Category2, setCategory2] = useState(false);
   const [Category3, setCategory3] = useState(false);
   const [Category4, setCategory4] = useState(false);
+  const [Category5, setCategory5] = useState(false);
 
   const [Primary1, setPrimary1] = useState("text-primary");
   const [Primary2, setPrimary2] = useState("");
   const [Primary3, setPrimary3] = useState("");
   const [Primary4, setPrimary4] = useState("");
+  const [Primary5, setPrimary5] = useState("");
 
   return (
     <div className="w-screen md:px-36 lg:px-96  bg-white overflow-none">
@@ -120,10 +129,12 @@ const Orders = () => {
                   setCategory2(false);
                   setCategory3(false);
                   setCategory4(false);
+                  setCategory5(false);
                   setPrimary1("text-primary");
                   setPrimary2("");
                   setPrimary3("");
                   setPrimary4("");
+                  setPrimary5("");
                 }}
               >
                 <IoCheckmarkCircleOutline
@@ -139,10 +150,12 @@ const Orders = () => {
                   setCategory2(true);
                   setCategory3(false);
                   setCategory4(false);
+                  setCategory5(false);
                   setPrimary1("");
                   setPrimary2("text-primary");
                   setPrimary3("");
                   setPrimary4("");
+                  setPrimary5("");
                 }}
               >
                 <BsBoxSeam className={`text-black h-6 w-full ${Primary2}`} />
@@ -156,10 +169,12 @@ const Orders = () => {
                   setCategory2(false);
                   setCategory3(true);
                   setCategory4(false);
+                  setCategory5(false);
                   setPrimary1("");
                   setPrimary2("");
                   setPrimary3("text-primary");
                   setPrimary4("");
+                  setPrimary5("");
                 }}
               >
                 <BsTruck className={`text-black h-6 w-full ${Primary3}`} />
@@ -173,16 +188,39 @@ const Orders = () => {
                   setCategory2(false);
                   setCategory3(false);
                   setCategory4(true);
+                  setCategory5(false);
                   setPrimary1("");
                   setPrimary2("");
                   setPrimary3("");
                   setPrimary4("text-primary");
+                  setPrimary5("");
                 }}
               >
                 <IoCheckmarkCircleSharp
                   className={`text-black h-6 w-full ${Primary4}`}
                 />
                 Completed
+              </div>
+              <div
+                className={`text-black py-3 text-sm text-center w-20 ${Primary5}`}
+                to="/Feed"
+                onClick={() => {
+                  setCategory1(false);
+                  setCategory2(false);
+                  setCategory3(false);
+                  setCategory4(false);
+                  setCategory5(true);
+                  setPrimary1("");
+                  setPrimary2("");
+                  setPrimary3("");
+                  setPrimary4("");
+                  setPrimary5("text-red-900");
+                }}
+              >
+                <IoCloseCircleSharp
+                  className={`text-black h-6 w-full ${Primary5}`}
+                />
+                Canceled
               </div>
             </div>
           </div>
@@ -229,11 +267,9 @@ const Orders = () => {
                                 {
                                   isCanceled: true,
                                   isShipped: true,
-                                  isReceivedFromSeller: true,
-                                  isReceivedFromCustomer: true,
-                                  isConfirmed:true,
                                 }
                               );
+                              refreshPage();
                             }}
                             className="w-40 text-white text-center bg-red-600 p-2 rounded-full"
                           >
@@ -485,6 +521,7 @@ const Orders = () => {
                                         isReceivedFromCustomer: true,
                                       }
                                     );
+                                    refreshPage();
                                   }}
                                   className=" w-70 text-white text-center bg-red-600 p-2 ml-40 rounded-3xl"
                                 >
@@ -623,21 +660,99 @@ const Orders = () => {
                             </div>
                           </div>
                           <div className=" flex-row">
-                            {isCanceled ? (
-                              <>
-                                <div className="float-left mr-1 font-bold text-red-800">
-                                  Your order has been cancel
-                                </div>
-                                <div className="float-left mr-1 font-bold text-red-800">
-                                  ,Please contact the seller for further
-                                  information
-                                </div>
-                              </>
-                            ) : (
-                              <div className="float-left mr-1 font-bold text-green-800">
-                                your order has been Completed
-                              </div>
-                            )}
+                            <div className="float-left mr-1 font-bold text-green-800">
+                              your order has been Completed
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              }
+            })}
+          </>
+        ) : (
+          ""
+        )}
+        {Category5 ? (
+          <>
+            {products?.map((product, index) => {
+              const {
+                StoreName,
+                description,
+                image,
+                price,
+                rate,
+                title,
+                type,
+                type_parameters,
+                id,
+                isConfirmed,
+                isShipped,
+                storePhoneNumber,
+                store_avatar,
+                isCanceled,
+              } = product;
+              if (isCanceled === true && isShipped === true) {
+                return (
+                  <>
+                    <div className="p-4 mx-4 my-2 flex border border-gray-400 rounded-xl ">
+                      <div className="w-full flex flex-col justify-between items-center">
+                        <div className="w-full flex justify-between items-center mb-4">
+                          <div className="w-full flex justify-start items-center">
+                            <img
+                              src={store_avatar}
+                              alt={StoreName}
+                              className="text-black h-10 w-10 rounded-full active:text-primary"
+                            />
+                            <p className="text-lg ml-4">{StoreName}</p>
+                          </div>
+                        </div>
+                        <div className="w-full flex flex-row justify-start items-center">
+                          <img
+                            src={image}
+                            alt={StoreName}
+                            className="text-black h-24 md:h-40 w-24  md:w-40 rounded active:text-primary"
+                          />
+                          <div className="w-full flex flex-col ml-12 items-start">
+                            <p className="text-xl font-medium ml-4">{title}</p>
+                            <div className="w-full flex flex-row justify-between ml-4 text-xl font-medium">
+                              <CurrencyFormat
+                                value={price}
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"RM "}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="flex flex-col justify-start items-start">
+                          <div className=" flex-row">
+                            <div className="float-left">
+                              <IoLocationOutline className="w-8 h-8" />
+                              Address :
+                            </div>
+                            <div className="float-right mt-8 ml-2 text-gray-500">
+                              {location}
+                            </div>
+                          </div>
+                          <div className=" flex-row">
+                            <div className="float-left mr-1">
+                              <IoChatbubblesOutline className="w-8 h-8" />
+                              Seller phone number:
+                            </div>
+                            <div className="float-right mt-8 ml-2 text-gray-500">
+                              {storePhoneNumber}
+                            </div>
+                          </div>
+                          <div className=" flex-row">
+                            <div className="float-left mr-1 font-bold text-red-800">
+                              Your order has been cancel
+                            </div>
+                            <div className="float-left mr-1 font-bold text-red-800">
+                              ,Please contact the seller for further information
+                            </div>
                           </div>
                         </div>
                       </div>
